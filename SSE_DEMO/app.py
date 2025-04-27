1c09aa817ae19edbf43e7e99d277174a2ceb7683
陈天霸 <s1yuan_chen@163.com>
2025-04-27 17:32:05 +0800

    转发流式结果

--- SSE_DEMO/app.py
+++ SSE_DEMO/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response
+from flask import Flask, Response, send_from_directory
 from flask_cors import CORS
 import time
 import json
+import requests
 
 app = Flask(__name__)
 CORS(app)  # 允许跨域请求
@@ -31,7 +32,7 @@ def stream2():
         }
         payload = {
             "messages": [
-                {"role": "user", "content": "今日北京天气如何？"}
+                {"role": "user", "content": "怎么种苹果？"}
             ],
             "stream": True
         }
@@ -39,12 +40,24 @@ def stream2():
         with requests.post(url, json=payload, headers=headers, stream=True) as response:
             for chunk in response.iter_content(chunk_size=None):
                 if chunk:
-                    yield f"data: {chunk.decode('utf-8')}\n\n"
+                    yield f"{chunk.decode('utf-8')}\n\n"
             # 发送结束事件
             yield "event: end\ndata: {}\n\n"
 
     return Response(generate(), content_type='text/event-stream')
 
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/index.html')
+def index_html():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/index2.html')
+def index2_html():
+    return send_from_directory('.', 'index2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
